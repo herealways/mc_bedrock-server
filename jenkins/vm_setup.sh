29df3c80e4e13cb5157d8f3d7f8f12575dfc7e7e
herealways <junqi1999@gmail.com>
2020-03-19 18:57:01 +0800

    Try 2 to fix issue of quoting variable in vm_setup.sh

--- jenkins/vm_setup.sh
+++ jenkins/vm_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if the vm is running
 state=$(vagrant global-status | awk '{if ($2=="centos7_mc" && $4=="running") print "running"}')
-if [ $state != "running" ];then
+if [ "$state" != "running" ];then
     cd Vagrant && vagrant up
 fi
